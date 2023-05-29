07a60a4f5f16546af15a5607185a3eb8371da9bb
Julio Cruz <julio.cruz@mercadolivre.com>
2023-05-29 18:13:33 -0300

    Add new logics to block and refactor in code

--- block.gd
+++ block.gd
@@ -1,15 +1,126 @@
-extends RigidBody2D
-class_name MovableBlock
+extends CharacterBody2D
+class_name Block
 
-var speed = 200
+@onready var rayCastRight = $RayCastRight
+@onready var rayCastLeft = $RayCastLeft
+@onready var rayCastTop = $RayCastTop
+@onready var rayCastBottom = $RayCastBottom
+
+const SPEED = 100 # Velocidade do bloco
+const SIZE = 128 # Tamanho do bloco
+const HALF_SIZE = (Block.SIZE / 2) # Meta do bloco
+
+# Possiveis spawns de X para os blocos
+var spawn_x = [ 
+	HALF_SIZE, 
+	HALF_SIZE * 3,
+	HALF_SIZE * 5, 
+	HALF_SIZE * 7, 
+	HALF_SIZE * 9, 
+	HALF_SIZE * 11, 
+	HALF_SIZE * 13, 
+	HALF_SIZE * 15,
+	HALF_SIZE * 17,
+	HALF_SIZE * 19,
+	]
+
+# Variáveis auxiliares para movimentação
+var can_push = true  # Indica se o bloco pode ser empurrado novamente
+var pushed_one_time = false # Indica se o bloco já foi empurrado pelo menos uma vez
+var last_direction_player # Indica a ultima direção que o player estava quando empurrou o bloco pela última vez
+var maxX = position.x # Indica o X máximo que o bloco deve alcançar após o "empurrão"
+var minX = position.x # Indica o X mínimo que o bloco deve alcançar após o "empurrão"
+
+var blockUp: Block # Indica o bloco que está em cima
+var blockDown: Block # Indica o bloco que está em baixo
+var blockRight: Block # Indica o bloco que está do lado direito
+var blockLeft: Block # Indica o bloco que está do lado esquerdo
+var color: int = 1 # Indica a cor do bloco
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var screen_size
 
 func _ready():
-	lock_rotation = true
-	angular_damp = 100.0
-	mass = 1.0 # Ajuste a massa do bloco para torná-lo mais leve
-	linear_damp = 0.0 # Ajuste o amortecimento linear para controlar o quão rápido o bloco para de se mover
-	
+	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	pass
+	# Se o bloco chegar ao topo o jogador perde
+	if is_on_floor() and position.y < HALF_SIZE:
+		print("YOU LOSE")
+		free()
 	
+	if pushed_one_time:
+		# Se a velocidade do bloco estiver zero ele continua se movendo até chegar no max X
+		if velocity.is_zero_approx():
+			velocity.x += SPEED * last_direction_player
+			move_and_slide()
+
+		position.x = clamp(position.x, minX, maxX)
+		if position.x >= maxX || position.x <= minX:
+			can_push = true
+
+func _physics_process(delta):
+	# Adicionando gravidade
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	# TODO: lógica para caso o bloco caia sobre outro bloco que já está se movimentando
+	# o bloco deve finalizar o movimento, e em seguida o bloco que está caindo deve cair
+	if rayCastBottom.is_colliding():
+		if rayCastBottom.get_collider() is Block:
+			var block = rayCastBottom.get_collider() as Block
+			if !block.can_push:
+				position.y -= 100
+		
+		# Se o bloco atingir a cabeça do jogador, ele perde
+		if rayCastBottom.get_collider() is Player:
+			print("YOU LOSE")
+
+
+	move_and_slide()
+
+# Direction: -1 = esquerda, 1 = direita
+func push(direction, playerX, playerY):	
+	if !can_push || !is_on_floor() || rayCastTop.is_colliding():
+		return
+		
+	# TODO:
+	# 1. Bug as vezes o bloco mesmo colidindo com o rayCast ele está deixando ser empurrado
+	
+	# Player está a cima do bloco
+	if (position.y - playerY) > SIZE:
+		return
+	
+	maxX = getNextX(position.x + SIZE/2 * direction, direction)
+	
+	# Empurrando para direita
+	#if position.x < maxX:	
+	if direction == 1:
+		if rayCastRight.is_colliding():
+			return
+		minX = position.x
+	# Empurrando para esquerda
+	else:
+		if rayCastLeft.is_colliding():
+			return
+		minX = maxX
+		maxX = position.x
+	
+	pushed_one_time = true
+	can_push = false
+	
+	last_direction_player = direction
+	velocity.x += SPEED * direction
+	
+	move_and_slide()
+
+func getNextX(positionX, direction):
+	if direction == 1:
+		for i in len(spawn_x):
+			if spawn_x[i] > positionX:
+				return spawn_x[i]
+	else:
+		for i in [9,8,7,6,5,4,3,2,1,0]:
+			print(spawn_x[i])
+			if spawn_x[i] < positionX:
+				return spawn_x[i]
--- player.gd
+++ player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
+class_name Player
+
+@onready var rayCastRight = $RayCastRight
+@onready var rayCastLeft = $RayCastLeft
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -600.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var screen_size
 
@@ -15,57 +18,44 @@ func _process(delta):
 	position.y = clamp(position.y, 0, screen_size.y)
 
 func _physics_process(delta):
-	#print(position.x)
-	# Add the gravity.
+	# Gravidade
 	if not is_on_floor():
-		#print(position.y)
 		velocity.y += gravity * delta
 		
-	## Flip
+	## Altera a direção dos sprites
 	if Input.is_action_pressed("move_right"):
 		$AnimatedSprite2D.flip_h = false
 	elif Input.is_action_pressed("move_left"):
 		$AnimatedSprite2D.flip_h = true
-
-	# Handle Jump.
+	
+	# Sprites de acordo com a movimentação e pulo
 	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("move_right") and is_on_floor():
-		$AnimatedSprite2D.play("move_right")
+		$AnimatedSprite2D.play("move")
 		
 	elif Input.is_action_pressed("move_left") and is_on_floor():
-		$AnimatedSprite2D.play("move_right")
+		$AnimatedSprite2D.play("move")
 	elif is_on_floor():
 		$AnimatedSprite2D.play("idle")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimento para direita e esquerda
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		
-		# print("Collided with: ", collision.get_collider().get_class())
-		var ismovable = collision.get_collider() is RigidBody2D
-		#print(ismovable)
-		if ismovable:
-			var movableBlock = collision.get_collider() as RigidBody2D
-			movableBlock.apply_central_impulse(Vector2(30 * direction, 0))
-			# movableBlock.apply_central_force(Vector2(10,0))
-		var isCharacterBlock = collision.get_collider() is CharacterBlock
-		if isCharacterBlock:
-			var movableBlock = collision.get_collider() as CharacterBlock
-			if movableBlock.blockDown != null:
-				var character = movableBlock.blockDown as CharacterBody2D
-				print(character.get_instance_id())
-				
-			movableBlock.moveX(direction, position.x, position.y)
+	
+	# Lógica de colisão com os blocos
+	if rayCastRight.is_colliding():
+		if rayCastRight.get_collider() is Block:
+			var block = rayCastRight.get_collider() as Block
+			block.push(1, position.x, position.y)
+	
+	if rayCastLeft.is_colliding():
+		if rayCastLeft.get_collider() is Block:
+			var block = rayCastLeft.get_collider() as Block
+			block.push(-1, position.x, position.y)
 
 	move_and_slide()
-
-
